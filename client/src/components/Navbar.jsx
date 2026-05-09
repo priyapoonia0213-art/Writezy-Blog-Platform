43f61885c4b1c9a1add640df241afa0ad5b6d332
bhawana kumari <bhawananehra12@gmail.com>
2026-05-09 15:38:20 +0530

    Adjust Navbar styling for improved layoutna

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     const { isDark, toggleTheme } = useTheme();
 
     return (
-        <div className='flex justify-between mt-[-80px] items-center py-5 mx-8 sm:mx-20 xl:mx-32'>
+       <div className='flex justify-between mt-[-50px] items-center py-5 px-2 sm:px-20 xl:px-32'>
            
             <img
                 onClick={() => navigate('/')}
